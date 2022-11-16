33b84f0861db062fcffba0d1ea00dfc9e0537417
Rohan Salvi <62870990+C4rohan@users.noreply.github.com>
2022-11-16 02:15:56 -0500

    Update kmediods.py

--- MWDC/clustering/K-Mediods/kmediods.py
+++ MWDC/clustering/K-Mediods/kmediods.py
@@ -1,5 +1,6 @@
 from sklearn_extra.cluster import KMedoids
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def kmedoids(X,data): # X is the number of n_clusters, 'data' is for data input
     
@@ -12,3 +13,20 @@ def kmedoids(X,data): # X is the number of n_clusters, 'data' is for data input
       print(frame['Cluster'].value_counts())
       #print("Silhouette Coefficient: %0.3f" % silhouette_score(x, labels))
       return frame,labels
+
+def optimalk(data):
+  cost =[]
+  for i in range(1, 11):
+      KM =KMedoids(n_clusters = i, metric='cosine',init='k-medoids++', max_iter = 500)
+      KM.fit(data)
+      
+      # calculates squared error
+      # for the clustered points
+      cost.append(KM.inertia_)    
+  
+  # plot the cost against K values
+  plt.plot(range(1, 11), cost, color ='g', linewidth ='3')
+  plt.xlabel("Value of K")
+  plt.ylabel("Squared Error (Cost)")
+   # clear the plot
+  return plt.show()
